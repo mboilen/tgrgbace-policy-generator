895f40262820ceb4236b33c10c2438f6aec9fed0
Mike Boilen <mgb@alumni.brown.edu>
2021-06-01 04:21:38 +0000

    cleanup code

--- tgrgbace-policy-generator.js
+++ tgrgbace-policy-generator.js
@@ -69,11 +69,6 @@ var sourcesObject = Object.entries(policies).map(function([key, value]) {
 var sourcesString = JSON.stringify(sourcesObject); 
 var sourcesKey = crypto.randomBytes(16).toString('hex');
 var encrypted = CryptoJS.AES.encrypt(sourcesString, sourcesKey).toString();
-//var sourcesJs = 
-    //'function getSources(key) {\n' +
-    //'\tvar str = CryptoJS.AES.decrypt(\'' + encrypted + '\', key).toString(CryptoJS.enc.Utf8);\n' +
-    //'\treturn JSON.parse(str);\n'+
-    //'}\n';
 var sourcesJs = 
     'function getSourcesString() {\n' +
 	'\treturn \'' + encrypted + '\';\n' + 
